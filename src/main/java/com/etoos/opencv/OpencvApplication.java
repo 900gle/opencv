72c9b62961b04438ce6a0c9b6c291a77cda0d60d
이두현 <doo@iduhyeon-ui-MacBookPro.local>
2020-10-20 10:12:16 +0900

    project init

---
+++ src/main/java/com/etoos/opencv/OpencvApplication.java
@@ -0,0 +1,13 @@
+package com.etoos.opencv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/etoos/opencv/OpencvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etoos.opencv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpencvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
